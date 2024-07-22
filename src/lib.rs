e718ce83df85298b95b5e43c1c0e8342c87ff41c
0xlunar <20531468+0xlunar@users.noreply.github.com>
2024-07-22 12:52:18 +1000

    Added doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+//! Easy to use configuration updater
+//! Automatically update your config when changes are made instead of restarting each time.
+//!
+//! # Example
+//! ```
+//! use serde::Deserialize;
+//! use config_updater::ConfigMonitor;
+//!
+//! #[derive(Deserialize)]
+//! struct MyConfig {
+//!     id: u64,
+//! }
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let config_monitor: ConfigMonitor<MyConfig> = ConfigMonitor::new("./config.json", Some(30));
+//!     let my_config = config_monitor.data(); // Arc<Mutex<MyConfig>>
+//!     let config_handle = config_monitor.monitor();
+//!
+//!     let c_my_config = my_config.clone();
+//!     tokio::spawn(async {
+//!         // Do Something with c_my_config
+//!         let my_id = {
+//!             let lock = c_my_config.lock().await;
+//!             lock.id.clone();
+//!         };
+//!
+//!         println!("My ID: {}", my_id);
+//!     });
+//!
+//!     config_handle.await.unwrap();
+//! }
+//! ```
+
 use std::fs::File;
 use std::sync::Arc;
 use std::io::BufReader;
